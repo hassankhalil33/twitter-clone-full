ccff133b456032fdec4041b29d16cd9ee01e629c
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-14 16:35:49 +0300

    Added Insert Query for New Users

--- backend/register.php
+++ backend/register.php
@@ -8,9 +8,13 @@ $lastName = $_POST["lastName"];
 $email = $_POST["email"];
 $dateJoined = date("d M Y @ " . "H" . ":i");
 $userType = 1;
-$passwordSS = hash("sha256", $_POST["password"] . $dateJoined . "asd");
+$password = hash("sha256", $_POST["password"] . $dateJoined . "asd");
 
-$query = $mysql
+$query = $mysql -> prepare(
+    "INSERT INTO users(user_type, f_name, l_name, username, password, email, date_of_join)
+    VALUE ($userType, ?, ?, ?, $password, ?, $dateJoined)");
+$query -> bind_param("ssss", $firstName, $lastName, $userName, $email);
+$query -> execute();
 
 echo json_encode($array);
 
