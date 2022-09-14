0bfc3453acbcbcae84690463aaba426261862679
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-15 00:33:02 +0300

    Created Token

---
+++ backend/token.php
@@ -0,0 +1,24 @@
+<?php
+
+// Init Variables
+
+$tokenHeader = {
+    "alg": "SHA256",
+    "typ": "JWT"
+};
+
+// Functions
+
+function tokenEncode() {
+
+};
+
+function tokenDecode() {
+
+};
+
+function verifySignature() {
+    
+};
+
+?>
