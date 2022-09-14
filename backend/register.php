9d21f8e4a1546356233d092e30662e1ab1f1a337
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-14 16:27:23 +0300

    Updated Registry API

--- backend/register.php
+++ backend/register.php
@@ -1,5 +1,17 @@
 <?php
 
+include("connection.php");
 
+$userName = $_POST["userName"];
+$firstName = $_POST["firstName"];
+$lastName = $_POST["lastName"];
+$email = $_POST["email"];
+$dateJoined = date("d M Y @ " . "H" . ":i");
+$userType = 1;
+$passwordSS = hash("sha256", $_POST["password"] . $dateJoined . "asd");
+
+$query = $mysql
+
+echo json_encode($array);
 
 ?>
