d392df6a003df3444fca5292ddb99eddc834a917
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-14 18:12:48 +0300

    Fixed Stringing Issue Returning False

--- backend/register.php
+++ backend/register.php
@@ -10,12 +10,27 @@ $dateJoined = date("d M Y @ " . "H" . ":i");
 $userType = 1;
 $password = hash("sha256", $_POST["password"] . $dateJoined . "asd");
 
+$check = $mysql -> prepare(
+    "SELECT username, email FROM users
+    WHERE username = '$userName' OR email = '$email'"
+);
+
+$check -> execute();
+$array = $check -> get_result();
+
+echo json_encode($array);
+
 $query = $mysql -> prepare(
-    "INSERT INTO users(user_type, f_name, l_name, username, password, email, date_of_join)
-    VALUE ($userType, ?, ?, ?, $password, ?, $dateJoined)");
+    "INSERT INTO users(user_type, f_name, l_name, username, `password`, email, date_of_joining)
+    VALUE ('$userType', ?, ?, ?, '$password', ?, '$dateJoined')");
+
+if ($query === FALSE) {
+    die ("Error: " . $mysql -> error);
+}
+
 $query -> bind_param("ssss", $firstName, $lastName, $userName, $email);
 $query -> execute();
 
-echo json_encode($array);
+echo json_encode("success");
 
 ?>
