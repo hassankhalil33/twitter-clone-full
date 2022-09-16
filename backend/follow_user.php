634ba1a10c6cfc75cd96263997eb242daccb2ca4
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-17 01:09:16 +0300

    Added Return ID Function

--- backend/follow_user.php
+++ backend/follow_user.php
@@ -3,7 +3,23 @@
 $userName = $_POST["userName"];
 $followed = $_POST["followed"];
 
+function returnId($user) {
+    $check = $mysql -> prepare(
+        "SELECT id FROM users
+        WHERE username = '$user'"
+    );
 
+    $check -> execute();
+    $array = $check -> get_result();
+
+    $response = [];
+    $response[] = $array -> fetch_assoc();
+
+    return $response;
+};
+
+$userId = returnId($userName);
+$followedId = returnId($followed);
 
 $query = $mysql -> prepare(
     "INSERT INTO follows(`user_id`, followed_user_id)
