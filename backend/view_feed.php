47e0bcd523993dcf9ae27bea871f65656dafdc83
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-17 22:30:09 +0300

    Added Functions, UserData and AllFollowings

--- backend/view_feed.php
+++ backend/view_feed.php
@@ -7,6 +7,40 @@ include("connection.php");
 
 $userName = $_POST["userName"];
 
+function getUserData($user, $mysql) {
+    $query = $mysql -> prepare(
+        "SELECT f_name, l_name FROM users
+        WHERE username = '$user'"
+    );
+    
+    $query -> execute();
+    $array = $query -> get_result();
+    
+    $response = [];
+    
+    while($i = $array -> fetch_assoc()) {
+        $response[] = $i;
+    };
 
+    return $response;
+};
+
+function getFollowing($user, $mysql) {
+    $query = $mysql -> prepare(
+        "SELECT followed_user_id FROM follows
+        WHERE `user_id` = '$user'"
+    );
+
+    $query -> execute();
+    $array = $query -> get_result();
+
+    $response = [];
+
+    while($i = $array -> fetch_assoc()){
+        $response[] = $i;
+    };
+
+    return $response;
+};
 
 ?>
