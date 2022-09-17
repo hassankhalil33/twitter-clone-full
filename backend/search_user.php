2ec6ece0ddc7b07699e23396886e0625d6296280
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-17 19:03:38 +0300

    Added Search Query Function

--- backend/search_user.php
+++ backend/search_user.php
@@ -3,6 +3,26 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
-incldue("connection.php");
+include("connection.php");
+
+$searchQuery = $_POST["searchQuery"];
+
+function searchFor($search, $mysql) {
+    $query = $mysql -> prepare(
+        "SELECT username, f_name, l_name FROM users
+        WHERE `username` = ? OR f_name = ? OR l_name = ?"
+    );
+
+    $query -> bind_param("sss", $search, $search, $search);
+    $query -> execute();
+    $array = $check -> get_result();
+
+    $response = [];
+    $response[] = $array -> fetch_assoc();
+
+    return $response
+};
+
+echo json_encode(searchFor($searchQuery, $mysql));
 
 ?>
