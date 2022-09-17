20ebeb2544b1bbbdbf1cc23a5a65d9e97dda5409
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-17 21:20:02 +0300

    Added Return Id Function, Tested

--- backend/post_tweet.php
+++ backend/post_tweet.php
@@ -5,4 +5,27 @@ header("Access-Control-Allow-Headers: *");
 
 include("connection.php");
 
+$userName = $_POST["userName"];
+$text = $_POST["text"];
+$datePosted = date("d M Y @ " . "H" . ":i");
+$images = $_POST["images"];
+
+function returnId($user, $mysql) {
+    $check = $mysql -> prepare(
+        "SELECT id FROM users
+        WHERE username = ?"
+    );
+
+    $check -> bind_param("s", $user);
+    $check -> execute();
+    $array = $check -> get_result();
+
+    $response = [];
+    $response[] = $array -> fetch_assoc();
+
+    return $response[0]["id"];
+};
+
+echo returnId($userName, $mysql);
+
 ?>
