ae500c33818206a1c168d062f298a5e9dcc46c77
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-17 11:09:42 +0300

    add functionality to signin btn - register page

--- frontend/js/register_scripts.js
+++ frontend/js/register_scripts.js
@@ -1,3 +1,19 @@
 window.onload = () => {
-  console.log("Hello!");
+  // variables
+  const registerSignin = document.getElementById("register-signin");
+  const signinContainer = document.getElementById("signin-container");
+  const loginSignupBtn = document.getElementById("login-signup-btn");
+
+  // Functions
+  const openSigninPopup = () => {
+    signinContainer.classList.toggle("popup-hidden");
+    console.log("open");
+  };
+  const closeSigninPopup = () => {
+    signinContainer.classList.toggle("popup-visible");
+  };
+
+  //
+  registerSignin.addEventListener("click", openSigninPopup);
+  loginSignupBtn.addEventListener("click", closeSigninPopup);
 };
