9a0bd6dc47d523472b17bcdbae40a4fffc85920f
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-17 17:04:31 +0300

    add funcionality to home tab in the navigation

--- frontend/js/home_scripts.js
+++ frontend/js/home_scripts.js
@@ -1,3 +1,30 @@
 window.onload = () => {
-  
+  // Variables
+  const navHome = document.getElementById("nav-home");
+  const navProfile = document.getElementById("nav-profile");
+  const homePage = document.getElementById("home-page");
+  const profilePage = document.getElementById("profile-page");
+
+  // Functions
+  const switchToHome = () => {
+    if (navHome.children[2].classList.contains("tab-not-selected")) {
+      console.log("psss");
+      navHome.children[0].classList.toggle("hidden");
+      navHome.children[1].classList.toggle("hidden");
+      navProfile.children[0].classList.toggle("hidden");
+      navProfile.children[1].classList.toggle("hidden");
+      navHome.children[2].classList.add("tab-selected");
+      navHome.children[2].classList.remove("tab-not-selected");
+      navProfile.children[2].classList.add("tab-not-selected");
+      navProfile.children[2].classList.remove("tab-selected");
+      homePage.classList.toggle("hidden");
+      profilePage.classList.toggle("hidden");
+    }
+  };
+
+  //
+  navHome.addEventListener("click", event => {
+    event.preventDefault();
+    switchToHome();
+  });
 };
