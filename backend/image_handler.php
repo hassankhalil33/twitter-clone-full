1f0c315f7e3f47ee0cdbfb9661b20646e4ffaf83
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-18 16:32:43 +0300

    Updated Image Handler Now Can Decode and Save in Server

--- backend/image_handler.php
+++ backend/image_handler.php
@@ -2,12 +2,15 @@
 
 include("connection.php");
 
-function imageDecode() {
-    return
+$image = $_POST["image"];
+
+function imageDecode($image) {
+    return base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $image)); //stackoverflow
 };
 
-function imageSave() {
-    return
+function imageSave($image, $id, $type) {
+    //file_put_contents("C:/xampp/htdocs/fswo5/twitter-clone/images/" . $type . $id . ".png", $image);
+    file_put_contents(dirname(__FILE__). "../images/" . $type . $id . ".png", $image);
 };
 
 ?>
